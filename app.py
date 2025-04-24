37b1825aeaf608043b563a509231b8e77299adf4
Mohamedag2001 <mohaansar2001@gmail.com>
2025-04-24 01:54:38 +0000

    ggggo

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__, static_folder='static')
 
+@app.route("/")
+def index():
+    return render_template("index.html")
 @app.route("/index/")
 def index():
     return render_template("index.html")
