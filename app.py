b9d7731cd9ad665b260088fe8fac6e20365ab104
Mohamedag2001 <mohaansar2001@gmail.com>
2025-04-24 01:11:06 +0000

    firt

--- app.py
+++ app.py
@@ -22,6 +22,32 @@ def appli():
 
 
 
+from flask import Flask , render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/index/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+@app.route('/produit')
+def produit():
+    return render_template('produit.html') 
+
+@app.route('/appli')
+def appli():
+    return render_template('appli.html') 
+
+if __name__ == "__main__":
+   app.run(debug=True)
 
 
 if __name__ == "__main__":
