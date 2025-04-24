c1ba827ab794ecd5b63325479305c51299c55416
Mohamedag2001 <mohaansar2001@gmail.com>
2025-04-24 01:44:14 +0000

    fff

--- app.py
+++ app.py
@@ -1,34 +1,6 @@
-from flask import Flask , render_template
+from flask import Flask, render_template
 
-app = Flask(__name__)
-
-@app.route("/index/")
-def index():
-    return render_template("index.html")
-
-@app.route("/about/")
-def about():
-    return render_template("about.html")
-
-
-@app.route('/produit')
-def produit():
-    return render_template('produit.html') 
-
-@app.route('/appli')
-def appli():
-    return render_template('appli.html') 
-
-
-
-
-from flask import Flask , render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("index.html")
+app = Flask(__name__, static_folder='static')
 
 @app.route("/index/")
 def index():
@@ -38,21 +10,13 @@ def index():
 def about():
     return render_template("about.html")
 
-@app.route('/produit')
+@app.route("/produit/")
 def produit():
-    return render_template('produit.html') 
+    return render_template("produit.html")
 
-@app.route('/appli')
+@app.route("/appli/")
 def appli():
-    return render_template('appli.html') 
+    return render_template("appli.html")
 
 if __name__ == "__main__":
-   app.run(debug=True)
-
-
-if __name__ == "__main__":
-   app.run(debug=True)
-
-
-
-    
+    app.run(debug=True)
